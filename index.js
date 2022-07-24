24ccf846b348fbe8e4de80198a46a225851b7950
Eric <ericlg.link@gmail.com>
2022-07-24 00:19:47 -0700

    Added function to add images to page

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
+
